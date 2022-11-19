ae2211f931328efa51dacd4a176443e8286d27b0
NailZinn <nailzinnurov17@gmail.com>
2022-11-19 23:06:13 +0300

    [update] server settings

--- Sport room/Local server/ServerSettings.cs
+++ Sport room/Local server/ServerSettings.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Local_server
+﻿namespace Local_server
 {
     internal class ServerSettings
     {
         public int Port => 7000;
-        public string Path => @"./site";
+        public string Path => @"./static/";
     }
 }
